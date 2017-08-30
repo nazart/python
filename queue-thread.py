2e207214082c0af0c4e9c1df4e5f8fa5a692c6b8
Eduardo Bustamante <ebustamante@contractor.ea.com>
2017-08-29 22:20:46 -0500

    Sugerencias

--- queue-thread.py
+++ queue-thread.py
@@ -4,19 +4,12 @@ import time
 import numpy as np
 def worker():
     while True:
-        item = q.get()
-        if item is None:
+        try:
+            item = q.get(False)
+            do_work(item)
+        except queue.Empty:
             break
-        do_work(item)
-        q.task_done()
 
-q = queue.Queue()
-threads = []
-num_worker_threads = 10
-for i in range(num_worker_threads):
-    t = threading.Thread(target=worker)
-    t.start()
-    threads.append(t)
 def do_work(item):
 	print('empieza el item', item)
 	a = np.random.randint(0,20)
@@ -27,14 +20,16 @@ def do_work(item):
 def source():
 	return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 
+q = queue.Queue()
 for item in source():
     q.put(item)
 
-# block until all tasks are done
-q.join()
-
-# stop workers
+threads = []
+num_worker_threads = 10
 for i in range(num_worker_threads):
-    q.put(None)
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
+
 for t in threads:
     t.join()
